740f636dad8083c89000896d45b51a0a011dd707
hanjoong.cho <hanjoongcho@gmail.com>
2017-12-27 02:14:59 +0900

    Refactoring

--- app/src/main/java/io/github/hanjoongcho/easypassword/activities/SettingsActivity.kt
+++ app/src/main/java/io/github/hanjoongcho/easypassword/activities/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +13,8 @@ import android.view.MenuItem
 import com.google.android.gms.drive.GoogleDriveDownloader
 import com.google.android.gms.drive.GoogleDriveUploader
 import com.simplemobiletools.commons.dialogs.RadioGroupDialog
+import com.simplemobiletools.commons.extensions.baseConfig
+import com.simplemobiletools.commons.extensions.isBlackAndWhiteTheme
 import com.simplemobiletools.commons.models.RadioItem
 import io.github.hanjoongcho.easypassword.R
 import io.github.hanjoongcho.easypassword.extensions.config
@@ -26,20 +29,19 @@ import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingsActivity : CommonActivity() {
     lateinit var res: Resources
+    private var linkColor = 0
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
         res = resources
+        linkColor = if (isBlackAndWhiteTheme()) Color.WHITE else baseConfig.primaryColor
         
         setSupportActionBar(toolbar)
         supportActionBar?.run {
             title = getString(R.string.settings)
             setDisplayHomeAsUpEnabled(true)
         }
-
-        initPreference()
-        bindEvent()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -57,6 +59,8 @@ class SettingsActivity : CommonActivity() {
         
         // handle option click
         setupFontSize()
+        setupPatternLock()
+        setupGoogleDrive()
     }
     
     private fun setupFontSize() {
@@ -75,53 +79,36 @@ class SettingsActivity : CommonActivity() {
                 initTextSize(findViewById(android.R.id.content), this@SettingsActivity);
             }
         }
+        settings_font_size_label.setTextColor(linkColor)
     }
 
+    private fun setupPatternLock() {
+        pattern_lock_holder.setOnClickListener {
+            val intent = Intent(this, PatternLockActivity::class.java)
+            intent.putExtra(PatternLockActivity.MODE, PatternLockActivity.SETTING_LOCK)
+            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, intent)
+        }
+        pattern_lock_label.setTextColor(linkColor)
+    }
+    
+    private fun setupGoogleDrive() {
+        google_drive_backup_holder.setOnClickListener {
+            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, GoogleDriveUploader::class.java)
+        }
+        google_drive_backup_label.setTextColor(linkColor)
+
+        google_drive_recovery_holder.setOnClickListener {
+            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, GoogleDriveDownloader::class.java)
+        }
+        google_drive_recovery_label.setTextColor(linkColor)
+    }
+    
     private fun getFontSizeText() = getString(when (config.fontSize) {
         FONT_SIZE_SMALL -> R.string.small
         FONT_SIZE_MEDIUM -> R.string.medium
         FONT_SIZE_LARGE -> R.string.large
         else -> R.string.extra_large
     })
-    
-    private fun bindEvent() {
-        pref1.setOnClickListener {
-            val intent = Intent(this, PatternLockActivity::class.java)
-            intent.putExtra(PatternLockActivity.MODE, PatternLockActivity.SETTING_LOCK)
-            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, intent)
-        }
-
-        pref2.setOnClickListener {
-            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, GoogleDriveUploader::class.java)
-        }
-
-        pref3.setOnClickListener {
-            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, GoogleDriveDownloader::class.java)
-        }
-
-        pref8.setOnClickListener {
-            val uri = Uri.parse("market://details?id=io.github.hanjoongcho.easypassword")
-            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
-            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-            try {
-                EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, goToMarket)
-            } catch (e: ActivityNotFoundException) {
-                val browser = Intent(Intent.ACTION_VIEW,
-                        Uri.parse("http://play.google.com/store/apps/details?id=io.github.hanjoongcho.easypassword"))
-                EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, browser)
-            }
-        }
-
-        pref9.setOnClickListener {
-            EasyPasswordHelper.startSettingActivityWithTransition(this@SettingsActivity, WebViewActivity.getStartIntent(this@SettingsActivity, getString(R.string.setting_license_url)))
-        }
-    }
-
-    private fun initPreference() {
-        prefSummary8.text = "Easy Password v ${getPackageVersion()}"
-    }
 
     private fun getPackageVersion(): String? {
 
