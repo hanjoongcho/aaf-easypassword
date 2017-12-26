8e5f44e2c82ddca8a341053e2d180fa4d87713aa
bulbarsaur <hanjoongcho@gmail.com>
2017-12-26 16:59:47 +0900

    Refactoring

--- app/src/main/java/io/github/hanjoongcho/easypassword/activities/AboutActivity.kt
+++ app/src/main/java/io/github/hanjoongcho/easypassword/activities/AboutActivity.kt
@@ -13,6 +13,9 @@ import com.simplemobiletools.commons.activities.LicenseActivity
 import com.simplemobiletools.commons.helpers.APP_LICENSES
 import com.simplemobiletools.commons.helpers.APP_NAME
 import com.simplemobiletools.commons.helpers.APP_VERSION_NAME
+import com.simplemobiletools.commons.extensions.baseConfig
+import com.simplemobiletools.commons.extensions.launchViewIntent
+import com.simplemobiletools.commons.extensions.isBlackAndWhiteTheme
 import io.github.hanjoongcho.easypassword.BuildConfig
 import io.github.hanjoongcho.easypassword.R
 import kotlinx.android.synthetic.main.activity_about.*
@@ -40,80 +43,80 @@ class AboutActivity : CommonActivity() {
         }
 
 //        appName = intent.getStringExtra(APP_NAME) ?: ""
-//        linkColor = if (isBlackAndWhiteTheme()) Color.WHITE else baseConfig.primaryColor
+        linkColor = if (isBlackAndWhiteTheme()) Color.WHITE else baseConfig.primaryColor
     }
 
     override fun onResume() {
         super.onResume()
 //        updateTextColors(about_holder)
 //
-//        setupWebsite()
-//        setupEmail()
-//        setupMoreApps()
-//        setupRateUs()
-//        setupInvite()
+        setupWebsite()
+        setupEmail()
+        setupMoreApps()
+        setupRateUs()
+        setupInvite()
 //        setupLicense()
 //        setupDonate()
 //        setupFacebook()
 //        setupGPlus()
         setupCopyright()
     }
-//
-//    private fun setupWebsite() {
-//        val websiteText = String.format(getString(R.string.two_string_placeholder), getString(R.string.website_label), getString(R.string.my_website))
-//        about_website.text = websiteText
-//    }
-//
-//    private fun setupEmail() {
-//        val label = getString(R.string.email_label)
-//        val email = getString(R.string.my_email)
-//
-//        val appVersion = String.format(getString(R.string.app_version, intent.getStringExtra(APP_VERSION_NAME)))
-//        val deviceOS = String.format(getString(R.string.device_os), Build.VERSION.RELEASE)
-//        val newline = "%0D%0A"
-//        val separator = "___________________"
-//        val body = "$newline$newline$newline$separator$newline${getString(R.string.additional_info)}:$newline$appVersion$newline$deviceOS"
-//        val href = "$label<br><a href=\"mailto:$email?subject=$appName&body=$body\">$email</a>"
-//        about_email.text = Html.fromHtml(href)
-//        about_email.movementMethod = LinkMovementMethod.getInstance()
-//    }
-//
-//    private fun setupMoreApps() {
-//        about_more_apps.setOnClickListener {
-//            launchViewIntent("https://play.google.com/store/apps/dev?id=9070296388022589266")
-//        }
-//        about_more_apps.setTextColor(linkColor)
-//    }
-//
-//    private fun setupInvite() {
-//        about_invite.setOnClickListener {
-//            val text = String.format(getString(R.string.share_text), appName, getStoreUrl())
-//            Intent().apply {
-//                action = Intent.ACTION_SEND
-//                putExtra(Intent.EXTRA_SUBJECT, appName)
-//                putExtra(Intent.EXTRA_TEXT, text)
-//                type = "text/plain"
-//                startActivity(Intent.createChooser(this, getString(R.string.invite_via)))
-//            }
-//        }
-//        about_invite.setTextColor(linkColor)
-//    }
-//
-//    private fun setupRateUs() {
+
+    private fun setupWebsite() {
+        val websiteText = String.format(getString(R.string.two_string_placeholder), getString(R.string.website_label), getString(R.string.my_website))
+        about_website.text = websiteText
+    }
+
+    private fun setupEmail() {
+        val label = getString(R.string.email_label)
+        val email = getString(R.string.my_email)
+
+        val appVersion = String.format(getString(R.string.app_version, intent.getStringExtra(APP_VERSION_NAME)))
+        val deviceOS = String.format(getString(R.string.device_os), Build.VERSION.RELEASE)
+        val newline = "%0D%0A"
+        val separator = "___________________"
+        val body = "$newline$newline$newline$separator$newline${getString(R.string.additional_info)}:$newline$appVersion$newline$deviceOS"
+        val href = "$label<br><a href=\"mailto:$email?subject=$appName&body=$body\">$email</a>"
+        about_email.text = Html.fromHtml(href)
+        about_email.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun setupMoreApps() {
+        about_more_apps.setOnClickListener {
+            launchViewIntent("https://play.google.com/store/apps/dev?id=9070296388022589266")
+        }
+        about_more_apps.setTextColor(linkColor)
+    }
+
+    private fun setupInvite() {
+        about_invite.setOnClickListener {
+            val text = String.format(getString(R.string.share_text), appName, getStoreUrl())
+            Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_SUBJECT, appName)
+                putExtra(Intent.EXTRA_TEXT, text)
+                type = "text/plain"
+                startActivity(Intent.createChooser(this, getString(R.string.invite_via)))
+            }
+        }
+        about_invite.setTextColor(linkColor)
+    }
+
+    private fun setupRateUs() {
 //        if (baseConfig.appRunCount < 5) {
 //            about_rate_us.visibility = View.GONE
 //        } else {
-//            about_rate_us.setOnClickListener {
-//                try {
-//                    launchViewIntent("market://details?id=$packageName")
-//                } catch (ignored: ActivityNotFoundException) {
-//                    launchViewIntent(getStoreUrl())
-//                }
-//            }
+            about_rate_us.setOnClickListener {
+                try {
+                    launchViewIntent("market://details?id=$packageName")
+                } catch (ignored: ActivityNotFoundException) {
+                    launchViewIntent(getStoreUrl())
+                }
+            }
 //        }
-//        about_rate_us.setTextColor(linkColor)
-//    }
-//
+        about_rate_us.setTextColor(linkColor)
+    }
+
 //    private fun setupLicense() {
 //        about_license.setOnClickListener {
 //            Intent(applicationContext, LicenseActivity::class.java).apply {
@@ -157,5 +160,5 @@ class AboutActivity : CommonActivity() {
         about_copyright.text = String.format(getString(R.string.copyright), versionName, year)
     }
 
-//    private fun getStoreUrl() = "https://play.google.com/store/apps/details?id=$packageName"
+    private fun getStoreUrl() = "https://play.google.com/store/apps/details?id=$packageName"
 }
